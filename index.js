d7585e2e44c844a891c2eef4daeb245e1f4720df
Ruben <rubecald2408@gmail.com>
2023-10-05 00:07:24 -0700

    Can now see user input in console

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-prompt(
+const userInputString = prompt(
     "Please enter some froyo flavors :3",
     "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
 )
+
+const stringArray = userInputString.split(",");
+
+console.log(userInputString)
