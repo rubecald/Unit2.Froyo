b648bd50dcd2214b754ea59fca3797f726fa2619
Ruben <rubecald2408@gmail.com>
2023-10-05 14:54:33 -0700

    Console returns flavor count

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+// 
+
+// prompts the user to input froyo flavors
 const userInputString = prompt(
-    "Please enter some froyo flavors :3",
+    "Please enter some froyo flavors :D",
     "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
 )
 
-const stringArray = userInputString.split(",");
+const flavors = userInputString.split(",");
+// console.log(userInputString)
 
-console.log(userInputString)
+// creates an object and goes through the flavors + updates the counts
+const flavorCount = {};
+
+for (const flavor of flavors) {
+    if(flavorCount.hasOwnProperty(flavor)) {
+        flavorCount[flavor]++;
+    } else {
+        flavorCount[flavor] = 1;
+    }
+    }
+    
+    // gives us the flavor counts(for some reason lists vanilla twice)
+    for (const flavor in flavorCount) {
+        if(flavorCount.hasOwnProperty(flavor)) {
+            console.log(`You entered "${flavor}" ${flavorCount[flavor]} time(s).`);
+        } 
+    }
